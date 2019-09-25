3a0ec71cc6d288919a9772979375c1168df895a9
arkuar <arttukuronen@gmail.com>
2019-09-25 16:58:21 +0300

    Osa 3a

---
+++ index.js
@@ -0,0 +1,96 @@
+const express = require('express')
+const app = express()
+
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+
+app.use(bodyParser.json())
+morgan.token('body', req => {
+  return JSON.stringify(req.body)
+})
+
+app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body', {
+  skip: (req, res) => { return req.method !== 'POST' }
+}))
+
+let persons = [
+  {
+    'name': 'Arto Hellas',
+    'number': '040-123456',
+    'id': 1
+  },
+  {
+    'name': 'Ada Lovelace',
+    'number': '39-44-5323523',
+    'id': 2
+  },
+  {
+    'name': 'Dan Abramov',
+    'number': '12-43-234345',
+    'id': 3
+  },
+  {
+    'name': 'Mary Poppendieck',
+    'number': '39-23-6423122',
+    'id': 4
+  }
+]
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(p => p.id === id)
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  res.status(204).end()
+})
+
+const generateId = () => {
+  const id = Math.floor(Math.random() * 1000000)
+  return id
+}
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+  if (!body.number) {
+    return res.status(400).json({
+      error: 'Number missing'
+    })
+  }
+
+  if (persons.some(person => person.name === body.name)) {
+    return res.status(400).json({
+      error: 'Name must be unique'
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  }
+  persons = persons.concat(person)
+  res.json(person)
+})
+
+app.get('/info', (req, res) => {
+  const length = persons.length
+  const date = new Date()
+  res.send(`<div><p>Phonebook has info for ${length} people</p><p>${date.toString()}</p></div>`)
+})
+
+const PORT = 3001
+app.listen(PORT)
